dcc3599d92a6c6b7778f6f9969646f810666283c
Miramirez22 <miramirez@unal.edu.co>
2022-05-09 12:20:17 -0500

    Update Camioneta.java

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -1,5 +1,21 @@
 package vehiculos;
 
 public class Camioneta extends Vehiculo {
+	private boolean volco;
+	static int CantidadCamionetas=0;
+	
+	public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
+		super(placa,puertas,90,nombre,precio,peso,"4X4",fabricante);
+		this.volco=volco;
+		CantidadCamionetas+=1;
+	}
+	
+	public void setPuestos(boolean volco) {
+		this.volco=volco;
+	}
+	
+	public boolean isVolco() {
+		return volco;
+	}
 
 }
